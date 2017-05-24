1af2d9983ba0841be83f2009c7344c635d63384b
David Kurth <david-kurth@sbcglobal.net>
2017-05-23 20:53:13 -0500

    adding more

--- swapi-mocha/test/test.js
+++ swapi-mocha/test/test.js
@@ -3,3 +3,12 @@ var expect = require("chai").expect;
 var request = require("request");
 var baseUrl = "https://swapi.co/api";
 var util = require("util");
+
+describe('return luke', function(){
+  it('returns luke skywalker', function(done){
+    request.get({ url: baseUrl + '/people/1/'}),
+      expect(response.statusCode).to.equal(200);
+      console.log(body);
+      done();
+  });
+});
